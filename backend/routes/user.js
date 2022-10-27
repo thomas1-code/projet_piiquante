9f3beb6b596772420880852c1eabe9d6dc1287b1
thomas1-code <thomas.o1@laposte.net>
2022-10-27 15:45:07 +0200

    ajout des middleware checkEmail et checkPassword sur signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const checkEmail = require("../middleware/checkEmail");
+const checkPassword = require("../middleware/checkPassword");
 const userCtrl = require("../controllers/user");
 
-router.post("/signup", userCtrl.signup);
+router.post("/signup", checkEmail, checkPassword, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
