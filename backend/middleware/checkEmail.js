07cc71183f92dde5e144ca07eb8b7be861bc4779
thomas1-code <thomas.o1@laposte.net>
2022-10-27 15:41:52 +0200

    ajout du middleware pour le contrôle de l'email

---
+++ backend/middleware/checkEmail.js
@@ -0,0 +1,10 @@
+const validator = require("validator");
+
+module.exports = (req, res, next) =>{
+    const email = req.body.email;
+    if(validator.isEmail(email)){
+        next();
+    }else{
+        return res.status(400).json({ error: "Email invalide !"})
+    }
+}
