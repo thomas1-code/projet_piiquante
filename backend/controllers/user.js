37218d0f4d21ab8919501988094b165065431b50
thomas1-code <thomas.o1@laposte.net>
2022-10-27 15:49:36 +0200

    ajout de crypto-js pour signup et login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,32 +1,36 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cryptoJs = require("crypto-js");
+require("dotenv").config();
 
-const User = require("../models/user");
+const User = require("../models/User");
 
 exports.signup = (req, res, next) =>{
+    const emailCryptoJs = cryptoJs.HmacSHA256(req.body.email, `${process.env.RANDOM_CRYPTOJS}`).toString();
     bcrypt.hash(req.body.password, 10)
         .then((hash) =>{
             const user = new User({
-                email: req.body.email,
+                email: emailCryptoJs,
                 password: hash
             })
             user.save()
                 .then (() => res.status(201).json( { message: "Votre compte a été créé !" }))
-                .catch((error) => res.status(400).json({ error}));
+                .catch((error) => res.status(400).json({ error }))
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error }))
 };
 
 exports.login = (req, res, next) =>{
-    User.findOne({ email: req.body.email })
+    const emailCryptoJs = cryptoJs.HmacSHA256(req.body.email, `${process.env.RANDOM_CRYPTOJS}`).toString();
+    User.findOne({ email: emailCryptoJs })
         .then((user) =>{
             if(!user){
-                return res.status(401).json({ message: "Email ou passeword incorrect" });
+                return res.status(401).json({ message: "Email ou passeword incorrect" })
             }
             bcrypt.compare(req.body.password, user.password)
                 .then((valid) =>{
                     if(!valid){
-                        return res.status(401).json({ message: "Email ou passeword incorrect" });
+                        return res.status(401).json({ message: "Email ou passeword incorrect" })
                     }
                     res.status(200).json({
                         userId: user._id,
@@ -37,7 +41,7 @@ exports.login = (req, res, next) =>{
                         )
                     });
                 })
-                .catch((error) => res.status(500).json({ error }));
+                .catch((error) => res.status(500).json({ error }))
         })
-        .catch((error) => res.status(500).json({ error }));
+        .catch((error) => res.status(500).json({ error }))
 };
